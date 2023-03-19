a37626dc0a9195dd8826cec63b1c17f3b249d7a2
herdiardn <herdiardan@gmail.com>
2023-03-19 20:14:19 +0700

    menambahkan class dataGeneric

--- Program.cs
+++ Program.cs
@@ -8,12 +8,30 @@ public class HaloGeneric
     }
 }
 
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data.ToString());
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
         HaloGeneric halo = new HaloGeneric();
-        string nama = "Kamal";
+        string nama = "Herdi Ardana";
         halo.SapaUser(nama);
+
+        DataGeneric<string> data = new DataGeneric<string>("1302210032");
+        data.PrintData();
     }
 }
