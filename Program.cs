d06938c2a2eaf35a2781b2ce663f70403974d7f8
herdiardn <herdiardan@gmail.com>
2023-03-19 19:28:02 +0700

    mengerjakan modul 5

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_1302210078
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO");
+        }
+    }
+}
